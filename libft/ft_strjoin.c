60bc0af95eec3a8ef05f5a61e09847e3afd56e3d
Valentin LECART <valecart@student.42.fr>
2019-04-23 12:48:03 +0200

    base push

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Valentin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/07 16:47:12 by Valentin          #+#    #+#             */
+/*   Updated: 2019/04/11 14:58:19 by valecart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char		*new;
+	size_t		i;
+	size_t		j;
+
+	i = 0;
+	j = 0;
+	if (!s1 && !s2)
+		return (NULL);
+	if (!(new = (char*)ft_memalloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
+		return (NULL);
+	if (s1)
+		while (s1[i])
+		{
+			new[i] = s1[i];
+			i++;
+		}
+	if (s2)
+		while (s2[j])
+		{
+			new[i] = s2[j];
+			i++;
+			j++;
+		}
+	new[i] = '\0';
+	return (new);
+}
