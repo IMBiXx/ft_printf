32f5ba4d1f1a8176e8af341f5f8956e81f80cb2c
Valentin LECART <valecart@student.42.fr>
2019-04-24 16:28:52 +0200

    add s option to io.c

--- src/io.c
+++ src/io.c
@@ -6,16 +6,18 @@
 /*   By: tpotier <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/23 18:34:02 by tpotier           #+#    #+#             */
-/*   Updated: 2019/04/24 16:07:24 by tpotier          ###   ########.fr       */
+/*   Updated: 2019/04/24 16:24:38 by valecart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
-
+#include <stdio.h> // remove pls
 void		ft_put_conv_spec(t_conv_spec *cspec, void *args, int *count)
 {
 	if (cspec->type == 'd')
 		*count += put_d(cspec, args);
+	if (cspec->type == 's')
+		*count += put_s(cspec, args);
 }
