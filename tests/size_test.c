d84a08cc162e63951bfc5b541484908d785a3d7a
Thomas POTIER <tpotier@e1r1p7.42.fr>
2019-04-24 19:15:27 +0200

    size

--- tests/size_test.c
+++ tests/size_test.c
@@ -3,6 +3,9 @@
 int	main(void)
 {
 	printf("void *: %lu\n", sizeof(void *));
+	printf("long double: %lu\n", sizeof(long double));
+	printf("double: %lu\n", sizeof(double));
+	printf("float: %lu\n", sizeof(float));
 	printf("long long: %lu\n", sizeof(long long));
 	printf("long: %lu\n", sizeof(long));
 	printf("int: %lu\n", sizeof(int));
