96c4e01f448d66a5948431683258fe2e740fd6f6
Thomas POTIER <tpotier@e1r9p20.42.fr>
2019-04-29 08:03:18 +0200

    example

--- main3.c
+++ main3.c
@@ -2,6 +2,9 @@
 
 int	main(void)
 {
-	ft_printf("%d\n", ft_printf("%kok%K\n", K_PURPLE | K_M_BOLD | K_M_OVER));
+	t_style mystyle;
+
+	mystyle = K_PURPLE | K_M_BOLD | K_M_UNDER | (K_YELLOW << K_BG_SHIFT);
+	ft_printf("%d\n", ft_printf("%k%lld%K\n", mystyle, 424242424242424242L));
 	return (0);
 }
