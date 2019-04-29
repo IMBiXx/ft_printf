860d96939ea3dbf08691b0076263b82caef4ed74
Valentin LECART <valecart@student.42.fr>
2019-04-29 16:45:48 +0200

    segfault protection for NULL parameter

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: valecart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/18 14:36:18 by valecart          #+#    #+#             */
-/*   Updated: 2019/04/29 07:54:32 by tpotier          ###   ########.fr       */
+/*   Updated: 2019/04/29 16:44:59 by valecart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int		ft_printf(const char *restrict format, ...)
 	va_list	arg;
 	int		ret;
 
+	if (!format)
+		return (-1);
 	va_start(arg, format);
 	ret = parse_print_format((char *)format, arg);
 	va_end(arg);
