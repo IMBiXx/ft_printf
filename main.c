9f960433dce7cdc6c2884f9ed5e4c0dee35a7db0
Valentin LECART <valecart@student.42.fr>
2019-04-29 15:35:20 +0200

    conv f fix :|

--- main.c
+++ main.c
@@ -3,16 +3,56 @@
 
 int	main(int ac, char **av)
 {
-	int	k;
+	/*int	k;*/
 
-	if (ac != 2)
-		return (-1);
-	ft_putstr("Mine: |");
-	k = ft_printf(av[1], 'A', 0, 42, 42.98765432101111, 42.0123456L, "hel\blo wo\xFFrld\n", 4242424242424242);
-	ft_putstr("| (");
-	ft_putnbr(k);
-	ft_putstr(")\nOrig: |");
-	k = printf(av[1], 'A', 0, 42, 42.98765432101111, 42.0123456L, "hello world", 4242424242424242);
-	printf("| (%d)\n", k);
+	/*if (ac != 2)*/
+		/*return (-1);*/
+	/*ft_putstr("Mine: |");*/
+	/*k = ft_printf(av[1], 'A', 45.123456789, 42, 42.98765432101111, 42.12349L, "hello world", 4242424242424242);*/
+	/*ft_putstr("| (");*/
+	/*ft_putnbr(k);*/
+	/*ft_putstr(")\nOrig: |");*/
+	/*k = printf(av[1], 'A', 45.123456789, 42, 42.98765432101111, 42.12349L, "hello world", 4242424242424242);*/
+	/*printf("| (%d)\n", k);*/
+	double		nb;
+	nb = -12547.58;
+
+	ft_printf("test basique:");
+    ft_printf("space:% f\n", nb);
+    ft_printf("plus:%+f\n", nb);
+    ft_printf("hash:%#f\n", nb);
+    ft_printf("precision:%.2f\n", nb);
+    ft_printf("big prec:%.14f\n", nb);
+    printf("big prec:%.14f\n", nb);
+    printf("big prec:%f\n", nb);
+    ft_printf("precision + hash:%#.0f\n", nb);
+    ft_printf("space + prec:% .5f\n", nb);
+    ft_printf("space + prec + hash:%# .0f\n", nb);
+    ft_printf("space + prec + hash:% #.0f\n", nb);
+    ft_printf("Plus + prec / grande:%+.5f\n", nb);
+    ft_printf("Plus + prec / petite:%+.0f\n", nb);
+    ft_printf("Plus + prec + hash:%#+.0f\n", nb);
+    ft_printf("Prec + 0:%0.5f\n", nb);
+    ft_printf("Prec + minus:%-.5f\n", nb);
+    ft_printf("size:%5f\n", nb);
+    ft_printf("size + space:% 5f\n", nb);
+    ft_printf("size + plus:%+5f\n", nb);
+    ft_printf("size + space:%# 5f\n", nb);
+    ft_printf("size + plus:%#+5f\n", nb);
+    ft_printf("size + minus:%-5f\n", nb);
+    ft_printf("size + 0:%05f\n", nb);
+    ft_printf("size + 0 + plus:%+05f\n", nb);
+    ft_printf("size + 0 + plus:%0+5f\n", nb);
+    ft_printf("size + 0 + prec:%05.3f\n", nb);
+    ft_printf("size + 0 + prec + hash:%0#5.0f\n", nb);
+    ft_printf("size + minus + prec:%-5.3f\n", nb);
+    ft_printf("size + minus + prec + hash:%-#5.0f\n", nb);
+    ft_printf("size + plus + 0 + prec:%+05.3f\n", nb);
+    ft_printf("size + plus + 0 + prec + hash:%0+#5.0f\n", nb);
+    ft_printf("size + espace + zero + prec:%0 5.3f\n", nb);
+    ft_printf("size + espace + zero + prec:% 05.3f\n", nb);
+    ft_printf("size + espace + zero + prec + hash:%#0 5.0f\n", nb);
+    ft_printf("size + minus + plus + prec:%-+5.3f\n", nb);
+    ft_printf("size + minus + plus + prec + hash:%-#+5.0f\n", nb);
 	return (0);
 }
--- src/io_f.c
+++ src/io_f.c
@@ -6,7 +6,7 @@
 /*   By: valecart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/24 15:53:35 by valecart          #+#    #+#             */
-/*   Updated: 2019/04/29 10:34:10 by valecart         ###   ########.fr       */
+/*   Updated: 2019/04/29 15:34:34 by valecart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 int			put_f(t_conv_spec *cs, va_list arg)
 {
 	long double		f;
+	long double		tmp;
 	size_t			num_size;
 	unsigned int	n;
 
@@ -32,10 +33,24 @@ int			put_f(t_conv_spec *cs, va_list arg)
 	n = 0;
 	if (cs->precision > 0)
 	{
-		f -= (int)f;
+		f -= (unsigned long long)f;
+		tmp = f;
+		while ((int)(n++) < cs->precision)
+			tmp = tmp * 10 - (((unsigned long long)tmp / 10) * 10);
+		if ((unsigned long long)tmp % 10 >= 5)
+		{
+			tmp = 1;
+			while (n-- > 1)
+				tmp /= 10;
+			f += tmp;
+		}
+		n = 0;
 		ft_putchar('.');
 		while ((int)(n++) < cs->precision)
-			ft_putchar('0' + (int)(f *= 10) % 10);
+		{
+			ft_putchar('0' + (unsigned long long)(f *= 10) % 10);
+			f -= (unsigned long long)f;
+		}
 	}
 	if (cs->flags & FLAG_M)
 		put_nchars(cs->field - num_size, ' ');
